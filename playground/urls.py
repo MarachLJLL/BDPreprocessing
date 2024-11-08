adba8dec18fb635e5552435bc66eaf7a6e15a65e
Luis Joseph Limgenco <marachljll@gmail.com>
2024-11-07 21:26:36 -0500

    Test pages

--- playground/urls.py
+++ playground/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 # URLConf module
 urlpatterns = [
-    path('hello', views.say_hello) 
+    path('hello', views.say_hello),
+    path('upload/', views.upload_file, name='upload_file'), 
 ]
--- playground/views.py
+++ playground/views.py
@@ -14,3 +14,20 @@ def say_hello(request):
     y = 2
     return render(request, 'hello.html', {'name': 'Mosh'})
     
+# views.py
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.core.files.storage import FileSystemStorage
+
+@csrf_exempt
+def upload_file(request):
+    return render(request, 'upload.html')
+    """
+    if request.method == 'POST' and request.FILES['file']:
+        uploaded_file = request.FILES['file']
+        fs = FileSystemStorage()
+        filename = fs.save(uploaded_file.name, uploaded_file)
+        file_url = fs.url(filename)
+        return JsonResponse({'file_url': file_url})
+    return JsonResponse({'error': 'Invalid request'}, status=400)
+    """
